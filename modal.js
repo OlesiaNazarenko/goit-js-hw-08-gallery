290544b95e7f3c15497ec599f9d20b001b11b891
OlesiaNazarenko <eightpa8@gmail.com>
2021-08-13 14:09:41 +0300

    Update modal.js

--- modal.js
+++ modal.js
@@ -1,7 +1,12 @@
 import galleryItems from './app.js';
 
 const gallery = document.querySelector(".gallery");
-const creatingGallery =  galleryItems.map(item => {
+const lightbox = document.querySelector('.js-lightbox');
+const lightboxImg = document.querySelector('.lightbox__image');
+const overlay = document.querySelector('.lightbox__overlay');
+const closeModalBtn = document.querySelector('.lightbox__button');
+
+const creatingGallery =  galleryItems.map((item, index) => {
     return `<li class="gallery__item" >
       <a class="gallery__link" href="${item.original}" >
       <img
@@ -9,17 +14,12 @@ const creatingGallery =  galleryItems.map(item => {
        src="${item.preview}"
        data-source="${item.original}";
        alt="${item.description}"
+       data-index = "${index}";
      />
       </a>
       </li>`;
   }).join('');
 gallery.insertAdjacentHTML('beforeend', creatingGallery);
-
-const lightbox = document.querySelector('.js-lightbox');
-const lightboxImg = document.querySelector('.lightbox__image');
-const overlay = document.querySelector('.lightbox__overlay');
-const closeModalBtn = document.querySelector('.lightbox__button');
-const slide = document.querySelector('.lightbox__content');
 
 function openModal(evt) {
     evt.preventDefault();
@@ -34,29 +34,50 @@ function openModal(evt) {
     }
   })
 }
-function closeModal(evt) {
-  if (evt.target.classList.contains('lightbox__button') || evt.target.classList.contains('lightbox__overlay') || evt.key === 'Escape') {
-    lightbox.classList.remove('is-open');
-    }
-    galleryItems.forEach(item => {
+function closeModal(evt) { 
+  if (evt.target.classList.contains('lightbox__button') || evt.target.classList.contains('lightbox__overlay') || evt.key === 'Escape' ) {
+      lightbox.classList.remove('is-open');
+      galleryItems.forEach(item => {
     if (evt.target.getAttribute('src') === item.original) {
       lightboxImg.removeAttribute('src');
-      lightboxImg.setAttribute('alt');
+        lightboxImg.setAttribute('alt');
     }
   })
-}
-
-
-// function nextSlide(evt) {
-//     evt.preventDefault();
-//     if (evt.target.classList.contains('lightbox__overlay') && evt.key === 'ArrowRight') {
-//         galleryItems.forEach((item, index) => { index >= item.length && (index != 0); index++ })
-//     }
+    }
     
+}
+// const keyP = function (evt) {
+//     const key = evt.code;
+//     switch(key){
+//         case "ArrowLeft":
+//             galleryItems.forEach((item,index, arr) => {
+//                 if (item.original === lightboxImg.src) {
+//                     if (index === 0) {
+//                         return;
+//                     }
+//                     lightboxImg.src = arr[index - 1].original;
+//                     return;
+//                 } 
+//             });
+//             break;
+//         case "ArrowRight":
+//             galleryItems.forEach((item,index, arr) => {
+//                 if (item.original === lightboxImg.src) {
+//                     if (index === arr.length - 1) {
+//                         return;
+//                     }
+                    
+//                     console.log(index)
+//                     lightboxImg.src = arr[index  + 1].original;
+//                     return;
+//                 } 
+//             });
+// break;
 // }
-
+// }
 gallery.addEventListener('click', openModal);
 closeModalBtn.addEventListener('click', closeModal);
 document.addEventListener('keydown', closeModal); 
 overlay.addEventListener('click', closeModal);
-// document.addEventListener('keydown', btnRight);
+overlay.addEventListener('click', closeModal);
+//document.addEventListener('keydown', keyP)
